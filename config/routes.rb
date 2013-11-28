78992cde8dcd05281a183a84efc8ca99d86339ac
Thomas Holmes <thomas@devminded.com>
2013-11-27 22:39:13 -0500

    Add index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ BattleFaces::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'application#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
