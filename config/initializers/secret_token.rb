39dacc04ae8030cacaf12d02441261381e9930ea
Thomas Holmes <thomas@devminded.com>
2013-11-27 22:12:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BattleFaces::Application.config.secret_key_base = '189f72e05aba7627692f10d395717e977f154cc636416dde7c2c04ed059d720733f2075c0eb272131bdd57b2ac5fe52dc51d7c813f57f6f89595e910871201a1'
