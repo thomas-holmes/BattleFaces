b25553f74e0cd2a7ca0f365435e3f0fe0f46a988
Thomas Holmes <thomas@devminded.com>
2013-11-28 23:46:00 -0500

    now master faces can login

--- app/assets/javascripts/controllers/sessions/new_controller.js
+++ app/assets/javascripts/controllers/sessions/new_controller.js
@@ -1,5 +1,11 @@
-// for more details see: http://emberjs.com/guides/controllers/
-
 BattleFaces.SessionsNewController = Ember.Controller.extend({
+  loginMasterFace: function() {
+    var router = this.get('target');
+    var data = this.getProperties('email', 'password');
 
+    $.post('/api/session', data, function(results) {
+      BattleFaces.AuthManager.authenticate(results.api_key.access_token, results.api_key.master_face_id);
+      router.transitionTo('index');
+    });
+  }
 });
--- app/assets/javascripts/routes/sessions/new_route.js
+++ app/assets/javascripts/routes/sessions/new_route.js
@@ -1,5 +1,5 @@
-// For more information see: http://emberjs.com/guides/routing/
-
 BattleFaces.SessionsNewRoute = Ember.Route.extend({
-
+  model: function() {
+    return Ember.Object.create();
+  }
 });
